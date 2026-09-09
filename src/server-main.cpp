c040b618fc6a34956bbd73117459b1fb9a3a9097
admin <popaopa21112@gmail.com>
2026-09-09 13:41:27 +0300

    add dark theme

--- src/server-main.cpp
+++ src/server-main.cpp
@@ -7,10 +7,35 @@
 
 #include <QApplication>
 #include <QJsonObject>
+#include <QStyleFactory>
 #include <QJsonDocument>
 
 int main(int argc, char** argv) {
    QApplication qapp(argc, argv);
+
+   QApplication::setStyle(QStyleFactory::create("Fusion"));
+
+   QPalette darkPalette;
+   darkPalette.setColor(QPalette::Window,          QColor(53, 53, 53));
+   darkPalette.setColor(QPalette::WindowText,      Qt::white);
+   darkPalette.setColor(QPalette::Base,            QColor(42, 42, 42));
+   darkPalette.setColor(QPalette::AlternateBase,   QColor(66, 66, 66));
+   darkPalette.setColor(QPalette::ToolTipBase,     Qt::white);
+   darkPalette.setColor(QPalette::ToolTipText,     Qt::white);
+   darkPalette.setColor(QPalette::Text,            Qt::white);
+   darkPalette.setColor(QPalette::Button,          QColor(53, 53, 53));
+   darkPalette.setColor(QPalette::ButtonText,      Qt::white);
+   darkPalette.setColor(QPalette::BrightText,      Qt::red);
+   darkPalette.setColor(QPalette::Link,            QColor(42, 130, 218));
+   
+   darkPalette.setColor(QPalette::Highlight,       QColor(42, 130, 218));
+   darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+   darkPalette.setColor(QPalette::Disabled, QPalette::WindowText, QColor(127, 127, 127));
+   darkPalette.setColor(QPalette::Disabled, QPalette::Text,       QColor(127, 127, 127));
+   darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, QColor(127, 127, 127));
+
+   QApplication::setPalette(darkPalette);
 
    net::Server server(2);
    if (!server.Start(net::global::PORT)) {
